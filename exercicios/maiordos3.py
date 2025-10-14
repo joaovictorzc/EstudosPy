248e8d8d054395072406c554b2a6ace1070f05f8
Victor <46138314+joaovictorzc@users.noreply.github.com>
2025-10-14 13:17:49 -0300

    23-26

---
+++ exercicios/maiordos3.py
@@ -0,0 +1,12 @@
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite um número: '))
+num3 = int(input('Digite um número: '))
+
+if num1 >= num2 and num1 >= num3:
+    maior = num1
+elif num2 >= num1 and num2 >= num3:
+    maior = num2
+else:
+    maior = num3
+
+print(f'O maior número é {maior}')
---
+++ exercicios/parouimpar.py
@@ -0,0 +1,8 @@
+numero = int(input('Digite um número: '))
+
+if numero == 0:
+    print('Zero')
+elif numero % 2 == 0:
+    print('Numero par')
+else:
+    print('Numero impar')
