9747bc3739abb4fdc8ace77788036813375c9237
Victor <46138314+joaovictorzc@users.noreply.github.com>
2026-06-22 15:54:01 -0300

    Update tipos.h

--- trabalho4/tipos.h
+++ trabalho4/tipos.h
@@ -1,59 +1,53 @@
 #include "sint.h"
 #include "stdio.h"
 
-static int hierarquia_tipo(int tipo){
-	switch(tipo){
-		case CHAR:
-			return 0;
-		case INT:
-			return 1;
-		case FLOAT:
-			return 2;
-		default:
-			return -1;
-	}
-}
-
 int retorna_maior_tipo(int tipo1, int tipo2){
-	int hierarquia1 = hierarquia_tipo(tipo1);
-	int hierarquia2 = hierarquia_tipo(tipo2);
+	/* CHAR -> INT -> FLOAT */
+	if(tipo1 == FLOAT || tipo2 == FLOAT)
+		return FLOAT;
 
-	if(hierarquia1 == -1 || hierarquia2 == -1)
-		return -1;
+	if(tipo1 == INT || tipo2 == INT)
+		return INT;
 
-	if(hierarquia1 >= hierarquia2)
-		return tipo1;
+	if(tipo1 == CHAR && tipo2 == CHAR)
+		return CHAR;
 
-	return tipo2;
+	return -1;
 }
 
 int get_tam_tipo(int tipo){
-	switch(tipo){
-		case CHAR:
-			return 1;
-		case INT:
-			return 4;
-		case FLOAT:
-			return 4;
-		case VOID:
-			return 0;
-		default:
-			return 0;
-	}
+    if(tipo == CHAR)
+        return 1;
+
+    if(tipo == INT)
+        return 4;
+
+    if(tipo == FLOAT)
+        return 4;
+
+    if(tipo == VOID)
+        return 0;
+
+    return 0;
 }
 
 int tipos_inconsistentes_atrib(int tipo1, int tipo2){
-	int hierarquia_destino = hierarquia_tipo(tipo1);
-	int hierarquia_origem = hierarquia_tipo(tipo2);
+    /* tipo1 = destino, tipo2 = origem */
 
-	if(tipo1 == tipo2)
-		return 0;
+    if(tipo1 == tipo2)
+        return 0;
 
-	if(hierarquia_destino == -1 || hierarquia_origem == -1)
-		return 1;
+    /* float recebe char, int e float */
+    if(tipo1 == FLOAT && (tipo2 == CHAR || tipo2 == INT || tipo2 == FLOAT))
+        return 0;
 
-	if(hierarquia_origem <= hierarquia_destino)
-		return 0;
+    /* int recebe char e int, mas nao float */
+    if(tipo1 == INT && (tipo2 == CHAR || tipo2 == INT))
+        return 0;
 
-	return 1;
+    /* char so recebe char */
+    if(tipo1 == CHAR && tipo2 == CHAR)
+        return 0;
+
+    return 1;
 }
